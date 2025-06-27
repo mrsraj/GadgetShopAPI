f7fe612e97f7d4c67b93f4140392b856d4ba4221
Sachin Raj <sachinraj6392@gmail.com>
2025-06-27 21:44:01 +0530

    add product model

--- gadgetApp/models.py
+++ gadgetApp/models.py
@@ -30,17 +30,6 @@ class Brand(models.Model):
     def __str__(self):
         return self.name
 
-# class Product(models.Model):
-#     name = models.CharField(max_length=200)
-#     description = models.TextField()
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     stock_quantity = models.IntegerField()
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-#     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-#     image_url = models.TextField(blank=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -52,8 +41,15 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # NEW FIELDS
+    is_latest = models.BooleanField(default=False)
+    is_best_seller = models.BooleanField(default=False)
+    is_new_arrival = models.BooleanField(default=False)
+    is_accessory = models.BooleanField(default=False)
+
     def __str__(self):
         return self.name
+
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
