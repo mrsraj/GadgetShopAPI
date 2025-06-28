36f71303641f7cacc5fe90ac319d8ce379a3418f
Sachin Raj <sachinraj6392@gmail.com>
2025-06-28 22:14:27 +0530

    add product releated sub-fun

--- gadgetApp/serializers.py
+++ gadgetApp/serializers.py
@@ -84,12 +84,70 @@ class ProductImageSerializer(serializers.ModelSerializer):
         model = ProductImage
         fields = ['id', 'product', 'image_url']
         
+# class ProductSerializer(serializers.ModelSerializer):
+#     images = ProductImageSerializer(many=True, read_only=True)
+
+#     class Meta:
+#         model = Product
+#         fields = ['id','name','description','price','stock_quantity','category','brand','created_at',
+#                   'updated_at',
+#             'images'  # nested images field
+#         ]
+
+
+###New Special code for product:
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'parent']
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ['id', 'name']
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'image_url']
+
+class ProductVariantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductVariant
+        fields = ['id', 'ram', 'processor', 'storage']
+
+class ProductPriceSerializer(serializers.ModelSerializer):
+    variant = ProductVariantSerializer(read_only=True)
+
+    class Meta:
+        model = ProductPrice
+        fields = ['id', 'variant', 'actual_price', 'offer_price', 'discount_percent', 'bought_last_month', 'rating', 'review_count']
+
+class ProductSpecificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductSpecification
+        fields = ['id', 'key', 'value']
+
+class ProductPolicySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductPolicy
+        fields = ['id', 'delivery_note', 'delivery_charge']
+
 class ProductSerializer(serializers.ModelSerializer):
     images = ProductImageSerializer(many=True, read_only=True)
+    variants = ProductVariantSerializer(many=True, read_only=True)
+    prices = ProductPriceSerializer(many=True, read_only=True)
+    specs = ProductSpecificationSerializer(many=True, read_only=True)
+    policies = ProductPolicySerializer(many=True, read_only=True)
+    category = CategorySerializer(read_only=True)
+    brand = BrandSerializer(read_only=True)
 
     class Meta:
         model = Product
-        fields = ['id','name','description','price','stock_quantity','category','brand','created_at',
-                  'updated_at',
-            'images'  # nested images field
+        fields = [
+            'id', 'name', 'description', 'stock_quantity',
+            'category', 'brand',
+            'created_at', 'updated_at',
+            'images', 'variants', 'prices', 'specs', 'policies'
         ]
