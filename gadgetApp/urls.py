5a97f0cd06fd874fc0e6a32480af6818fc159be6
Sachin Raj <sachinraj6392@gmail.com>
2025-07-05 00:23:22 +0530

    modify and add new

--- gadgetApp/urls.py
+++ gadgetApp/urls.py
@@ -2,17 +2,7 @@ from django.urls import path
 from .views import RegisterView,LoginView,ProtectedView,AdminOnlyView,TeacherOnlyView,StudentOnlyView
 from rest_framework_simplejwt.views import TokenRefreshView,TokenObtainPairView
 
-from .views import(product_list, product_detail,cart_list,
-                   cart_detail,address_list, 
-                   address_detail,order_list, order_detail,
-                   orderitem_list, orderitem_detail,
-                   payment_list, payment_detail,
-                   category_list,category_detail,
-                   brand_list,brand_detail,
-                   cartitem_list,cartitem_detail,
-                   product_image_list, product_image_detail
-                )
-
+from . import views
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -27,39 +17,21 @@ urlpatterns = [
     path('student-only/', StudentOnlyView.as_view(), name='student_only'),
     
     ## New Code
-    path('categories/', category_list, name='category-list'),
-    path('categories/<int:pk>/', category_detail, name='category-detail'),
     
-    path('brands/', brand_list, name='brand-list'),
-    path('brands/<int:pk>/', brand_detail, name='brand-detail'),
-    
-    path('products/', product_list, name='brand-list'),
-    path('products/<int:pk>/', product_detail, name='brand-detail'),
-    
-    path('carts/', cart_list, name='cart-list'),
-    path('carts/<int:pk>/', cart_detail, name='cart-detail'),
-    
-    path('addresses/', address_list, name='address-list'),
-    path('addresses/<int:pk>/', address_detail, name='address-detail'),
-    
-    path('orders/', order_list, name='order-list'),
-    path('orders/<int:pk>/', order_detail, name='order-detail'),
-    
-    path('order-items/', orderitem_list, name='orderitem-list'),
-    path('order-items/<int:pk>/', orderitem_detail, name='orderitem-detail'),
-    
-    path('payments/', payment_list, name='payment-list'),
-    path('payments/<int:pk>/', payment_detail, name='payment-detail'),
-    
-    path('cart-items/', cartitem_list, name='cartitem-list'),
-    path('cart-items/<int:pk>/', cartitem_detail, name='cartitem-detail'),
-    
-    path('product-images/', product_image_list, name='productimage-list'),
-    path('product-images/<int:pk>/', product_image_detail, name='productimage-detail'),
-    
-    path('products/', product_list, name='product-list'),
+    path('products/', views.product_list, name='product-list'),
+    path('products/<int:pk>/', views.product_detail, name='product-detail'),
 ]
 
 
 ## """Create items in order: Category → 
 # Brand → Product → Cart → CartItem → Address → Order → OrderItem → Payment."""
+
+# /products/?ram=8GB
+
+# /products/?storage=512GB
+
+# /products/?brand=HP
+
+# /products/?min_price=30000&max_price=60000
+
+# /products/?ram=16GB&brand=Lenovo&min_price=40000
