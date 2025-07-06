b9b8bb1e3ee604205679783b0fe2ebaded0aeab1
Sachin Raj <sachinraj6392@gmail.com>
2025-07-06 10:44:26 +0530

    model register

--- gadgetApp/admin.py
+++ gadgetApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ProductImage,Product,ProductSpecification,ProductPolicy
+
+admin.site.register(ProductImage)
+admin.site.register(Product)
+admin.site.register(ProductSpecification)
+admin.site.register(ProductPolicy)
