a267a7797fe7dd20399a302a97b3a142ea873222
Sachin Raj <sachinraj6392@gmail.com>
2025-07-09 00:09:29 +0530

    add create product urls

--- gadgetApp/urls.py
+++ gadgetApp/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('products/', views.product_list, name='product-list'),
     path('products/<int:pk>/', views.product_detail, name='product-detail'),
     path('product-images/create/', views.create_product_image, name='product-image-create'),
+    path('products/create/', views.product_create, name='product-create'),
 ]
 
 
